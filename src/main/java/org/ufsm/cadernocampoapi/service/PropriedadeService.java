d098053c0b28cc06819f153854f74518456f5177
Iuri <iuriguilhermesantos@gmail.com>
2026-04-30 18:02:00 -0300

    mutias coisas

--- src/main/java/org/ufsm/cadernocampoapi/service/PropriedadeService.java
+++ src/main/java/org/ufsm/cadernocampoapi/service/PropriedadeService.java
@@ -1,21 +1,51 @@
 package org.ufsm.cadernocampoapi.service;
 
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.stereotype.Service;
+import org.ufsm.cadernocampoapi.dto.PropriedadeRequestDTO;
+import org.ufsm.cadernocampoapi.model.Produtor;
+import org.ufsm.cadernocampoapi.model.ProdutorPropriedade;
 import org.ufsm.cadernocampoapi.model.Propriedade;
+import org.ufsm.cadernocampoapi.repositories.ProdutorRepository;
+import org.ufsm.cadernocampoapi.repositories.PropriedadeRepository;
+
+import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PropriedadeService {
 
-    private void criarPropriedade() {
+    private final ProdutorRepository produtorRepository;
+    private final PropriedadeRepository propriedadeRepository;
+
+
+    @Transactional
+    public void criarPropriedade(PropriedadeRequestDTO propriedadeRequestDTO) {
+
         Propriedade prop = Propriedade.builder()
-                .nome("Sítio do Pica-Pau")
-                .cep("97000-000")
-                .cidade("Santa Maria")
-                .estado("RS")
-                .areaTotal(50.0)
-                .areaPropria(50.0)
+                .nome(propriedadeRequestDTO.getNome())
+                .cep(propriedadeRequestDTO.getCep())
+                .cidade(propriedadeRequestDTO.getCidade())
+                .estado(propriedadeRequestDTO.getEstado())
+                .areaTotal(propriedadeRequestDTO.getAreaTotal())
+                .areaPropria(propriedadeRequestDTO.getAreaPropria())
+                .areaArrendada(propriedadeRequestDTO.getAreaArrendada())
+                .areaProducaoVegetal(propriedadeRequestDTO.getAreaProducaoVegetal())
                 .build();
+
+        Produtor produtor = produtorRepository.findById(5L)
+                .orElseThrow(() -> new RuntimeException("Produtor não encontrado"));
+
+        ProdutorPropriedade relacao = new ProdutorPropriedade();
+        relacao.setPropriedade(prop);
+        relacao.setProdutor(produtor);
+        relacao.setPapel("PROPRIETARIO");
+        prop.setProdutores(List.of(relacao));
+
+        propriedadeRepository.save(prop);
     }
 
 
