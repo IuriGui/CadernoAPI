4aa8eed054b51252fb85286cf03cd2ebbb5a5d76
IuriGui <iuriguilhermesantos@gmail.com>
2026-05-08 17:38:16 -0300

    feat: implementa base security

---
+++ src/main/java/org/ufsm/cadernocampoapi/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package org.ufsm.cadernocampoapi.security;
+
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.ufsm.cadernocampoapi.model.Usuario;
+import org.ufsm.cadernocampoapi.repositories.UsuarioRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario u = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario nao encontrado"));
+
+        return new User(
+                u.getEmail(),
+                u.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_PRODUTOR"))
+        );
+
+    }
+}
